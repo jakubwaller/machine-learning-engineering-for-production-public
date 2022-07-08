a1ea27ccff8f3758ba1fc9646224426184b4e206
Jakub Waller <jakub.waller@protonmail.com>
2022-07-08 13:29:19 +0200

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Testing the CI/CD pipeline
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
